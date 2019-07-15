5ad44e18f4554b4312f3c2d521472e13b37caa3a
Ben Eisenberg <ben.iceberg@gmail.com>
2019-07-15 17:17:06 +0200

    add atlas db

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const mongodbUri = `mongodb://localhost/ikea-gcp-db`;
+const mongodbUri = `mongodb+srv://${process.env.DB_USERNAME}:${
+  process.env.DB_PASSWORD
+}@clustercustomers-d3x22.gcp.mongodb.net/test?retryWrites=true&w=majority/ikea-gcp-db`;
 
 mongoose
   .connect(mongodbUri, { useNewUrlParser: true })
